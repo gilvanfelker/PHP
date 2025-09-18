c53b39b601bbae612ba53c80e9009f67e96d758a
gilvanfelker <120064511+gilvanfelker@users.noreply.github.com>
2025-09-18 16:04:49 -0300

    Update Usuario.php

--- model/Usuario.php
+++ model/Usuario.php
@@ -1,17 +1,19 @@
-<?php 
+<?php
 class Usuario {
     public $id_usuario;
     public $nm_usuario;
     public $email_usuario;
     public $pwd_usuario;
+    public $email_contato;
+    public $numero_telefone;
+    
 
-    public function __construct( $nm_usuario = null, $email_usuario = null, $pwd_usuario = null) {
+    public function __construct( $id_usuario = null, $nm_usuario = null, $email_usuario = null, $pwd_usuario = null, $email_contato = null, $numero_telefone = null) {
+       $this->id_usuario = $id_usuario;
         $this->nm_usuario   = $nm_usuario;
         $this->email_usuario = $email_usuario;
         $this->pwd_usuario  = $pwd_usuario;
+        $this->email_contato  = $email_contato;
+        $this->numero_telefone  = $numero_telefone;
     }
-
-    // public function setUsuario(){
-    //     $this->id_usuario = $id_usuario;
-    // }
 }
